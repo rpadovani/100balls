013ad2ec6e52b363c88f74ac80ae10ef9ac143f4
Riccardo Padovani <rpadovani@ubuntu.com>
2014-07-15 23:59:03 +0200

    Renamed ballcomponent - fixed setup.js

--- js/setup.js
+++ js/setup.js
@@ -19,7 +19,7 @@
 function newGame() {
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
-            var newBox = ballComponent.createObject(scene.world);
+            var newBox = ball.createObject(scene.world);
             newBox.x = width / 3 + units.gu(1.5*j);
             newBox.y = units.gu(1.5*i);
         }
